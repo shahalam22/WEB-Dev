263dd90a5735813b202aabfb9a5d20327e1b1aa1
Shah Alam Abir <shahalamabir21@gmail.com>
2023-09-21 15:53:40 +0600

    from home

--- Stack Learner/Practice Code/lecture_22/test.js
+++ Stack Learner/Practice Code/lecture_22/test.js
@@ -2,6 +2,30 @@
 
 const mongoose = require('mongoose');
 
+
+// "node test.js" diye eta run korle error dekhabe jodi database running na thake.
+// database run korate "mongod" command use korte hobe.
+
 mongoose.connect('mongodb://localhost:27017/test',{
     serverSelectionTimeoutMS: 1000,
+}).then(()=>{
+    console.log("Database connected");
+    createUser({name: 'Ayman', email: 'ayman@example.com'});
+    createUser({name: 'Abir', email: 'abir@example.com'});
+    
+}).catch((e) => {
+    console.log(e);
 });
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+async function createUser(data){
+    const user = new User(...data);
+    await user.save();
+}
