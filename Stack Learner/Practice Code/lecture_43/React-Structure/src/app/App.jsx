d000bc30e001446a5a888da4d48280bebef27726
SAAbir <shahalamabir21@gmail.com>
2024-01-06 11:36:32 +0600

    update on lecture 43

--- Stack Learner/Practice Code/lecture_43/React-Structure/src/app/App.jsx
+++ Stack Learner/Practice Code/lecture_43/React-Structure/src/app/App.jsx
@@ -1,7 +1,15 @@
+import InputGroups from "../components/shared/forms/InputGroups";
+import Button from "../components/ui/buttons/Button";
+import TextInput from "../components/ui/inputs/TextInput";
+import Text from "../components/ui/texts/Text";
+
 const App = () => {
     return(
         <div>
             <h1>App</h1>
+            <div>
+                <InputGroups name='title' placeholder='Enter the title' label='Title' error='Something went wrong'/>
+            </div>
         </div>
     )
 }
---
+++ Stack Learner/Practice Code/lecture_43/React-Structure/src/components/shared/forms/InputGroups.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import TextInput from "../../ui/inputs/TextInput"
+import Label from "../../ui/inputs/Label";
+
+const Container = styled.div`
+    width: 90%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+`
+
+const ErrorMessage = styled.div`
+    font-size: 0.8rem;
+    color: red;
+    font-family: Arial;
+`
+
+export const InputGroups = ({label, name, value, placeholder, error, onChange, onFocus, onBlur}) => {
+  return (
+    <Container>
+        <Label htmlFor = {name}>{label}</Label>
+        <TextInput name={name} id={name} placeholder={placeholder}/>
+        {error && <ErrorMessage>{error}</ErrorMessage>}
+    </Container>
+  )
+}
+
+
+export default InputGroups;
